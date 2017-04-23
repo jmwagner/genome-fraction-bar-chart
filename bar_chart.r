490492e7d5ebc6d0cfcc505adfdd03f7b25fcebc
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2017-04-23 13:55:33 +0200

    feature(init): initial code

---
+++ bar_chart.r
@@ -0,0 +1,42 @@
+#!/usr/bin/Rscript
+
+library(plyr)
+library(ggplot2)
+library(plotly)
+
+args = commandArgs(trailingOnly=TRUE)
+path = args[1] 
+postfix = args[2]
+
+inputDataframe = cbind.data.frame(read.delim(path, header = TRUE, stringsAsFactors =
+                                               FALSE))
+inputDataframe$path <- file.path(inputDataframe$path, postfix)
+
+fillDataset <- function(paths) {
+   for (file in paths) {
+            if (!exists("dataset")) {
+	        print(file)
+       		dataset <- cbind.data.frame(read.delim(file, header=TRUE, stringsAsFactors=FALSE))
+	        dataset <- dataset[!grepl("broken", dataset$Assembly),]
+	        dataset$path = file
+            } else {
+              print(file)
+              temp_dataset <- cbind.data.frame(read.delim(file, header=TRUE, stringsAsFactors=FALSE))
+              temp_dataset$path = file
+              temp_dataset <- temp_dataset[!grepl("broken", temp_dataset$Assembly),]
+              dataset <- rbind.fill(list(dataset, temp_dataset))
+              rm(temp_dataset)
+            }
+  }
+  dataset
+}
+
+total <- merge(inputDataframe, fillDataset(inputDataframe$path), by = c("path"))
+total[is.na(total)] <- 0
+
+p <- ggplot(data=total, aes(x=name, y=Genome.fraction...., fill=name)) +
+    geom_bar(stat="identity") + coord_flip()
+p <- ggplotly(p)
+
+htmlwidgets::saveWidget(p,'/output/out.html')
+system("cp /project/biobox.yaml /output")
